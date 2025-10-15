b46ecf8f9431f83bbb76f3258533cceaadb5754e
Dan Gowans <dangowans@users.noreply.github.com>
2025-10-15 14:50:19 -0400

    clearer postinstall output

--- postinstall.ts
+++ postinstall.ts
@@ -7,14 +7,22 @@ import {
 } from '@cityssm/puppeteer-launch'
 
 try {
-  await testInstalledChromeBrowser(true)
-  console.log('✔️  Chrome browser installed successfully')
+  const result = await testInstalledChromeBrowser(true)
+  console.log(
+    result.ranInstaller
+      ? '✔️  Chrome browser installed successfully.'
+      : '✔️  Chrome browser already installed.'
+  )
 } catch (error) {
   console.error('❌  Error installing Chrome browser:', error)
 
   try {
-    await testInstalledFirefoxBrowser(true)
-    console.log('✔️  Firefox browser installed successfully')
+    const result = await testInstalledFirefoxBrowser(true)
+    console.log(
+      result.ranInstaller
+        ? '✔️  Firefox browser installed successfully.'
+        : '✔️  Firefox browser already installed.'
+    )
   } catch (error) {
     console.error('❌  Error installing Firefox browser:', error)
   }
