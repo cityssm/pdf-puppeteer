8c79224eec37c1dc62353f3e512240dde6f27615
Dan Gowans <dangowans@users.noreply.github.com>
2025-10-15 14:45:33 -0400

    only install is unavailable

--- postinstall.ts
+++ postinstall.ts
@@ -1,16 +1,19 @@
 // eslint-disable-next-line @eslint-community/eslint-comments/disable-enable-pair
 /* eslint-disable no-console */
 
-import { installChromeBrowser, installFirefoxBrowser } from '@cityssm/puppeteer-launch'
+import {
+  testInstalledChromeBrowser,
+  testInstalledFirefoxBrowser
+} from '@cityssm/puppeteer-launch'
 
 try {
-  await installChromeBrowser()
+  await testInstalledChromeBrowser(true)
   console.log('✔️  Chrome browser installed successfully')
 } catch (error) {
   console.error('❌  Error installing Chrome browser:', error)
 
   try {
-    await installFirefoxBrowser()
+    await testInstalledFirefoxBrowser(true)
     console.log('✔️  Firefox browser installed successfully')
   } catch (error) {
     console.error('❌  Error installing Firefox browser:', error)
